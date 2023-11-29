3082a4041800cad203401816e014b5be981900f1
BOUZELBOUDJEN Med Abdelwahab <148821529+waaahaaab@users.noreply.github.com>
2023-11-29 15:53:18 +0100

    Addedupdate

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -11,25 +11,25 @@ const Container = styled.div`
   padding: 20px;
 `;
 function Todos() {
-  const [credentials]= useContext(CredentialsContext);
-  const [Todos, setTodos]=useState([]);
+  const [credentials] = useContext(CredentialsContext);
+  const [Todos, setTodos] = useState([]);
   const handleAddTodo = (newTodo) => {
     setTodos([...Todos, newTodo]);
   };
 
-  const LeDelete= (id) =>{
+  const LeDelete = (id) => {
 
-    const ID={id};
+    const ID = { id };
     fetch("http://localhost:4000/todosdelete", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Basic ${credentials.username}:${credentials.password}`,
-      //  ...(todo._id ? { Id: `Basic ${todo._id}` } : {})
+        //  ...(todo._id ? { Id: `Basic ${todo._id}` } : {})
       },
-      body:JSON.stringify(ID),
+      body: JSON.stringify(ID),
     })
-    .then(()=>{Fetcher()});
+      .then(() => { Fetcher() });
 
   }
 
@@ -39,28 +39,28 @@ function Todos() {
     //Fetcher();
 
   };
-  
-  const Fetcher=()=>{
+
+  const Fetcher = () => {
     console.log("sent");
-      fetch("http://localhost:4000/todos", {
+    fetch("http://localhost:4000/todos", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Basic ${credentials.username}:${credentials.password}`,
       },
     })
-    //.then(handleErrors)
-    .then(response => response.json())
-  .then(newTodos => {
-    // setTodos(prevTodos => [...prevTodos, ...newTodos]);
-    setTodos(newTodos);
-  });
+      //.then(handleErrors)
+      .then(response => response.json())
+      .then(newTodos => {
+        // setTodos(prevTodos => [...prevTodos, ...newTodos]);
+        setTodos(newTodos);
+      });
   }
 
 
-  const LePasseur=(newTodos)=>{
-      //console.log("sent");
-      fetch("http://localhost:4000/todos", {
+  const LePasseur = (newTodos) => {
+    //console.log("sent");
+    fetch("http://localhost:4000/todos", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -68,49 +68,69 @@ function Todos() {
       },
       body: JSON.stringify(newTodos),
     })
-    //.then(handleErrors)
-    .then(()=>{Fetcher()});
+      //.then(handleErrors)
+      .then(() => { Fetcher() });
   }
-  const LeChecker=(todo)=>{
+  const LeChecker = (todo) => {
     fetch("http://localhost:4000/todoscheck", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Basic ${credentials.username}:${credentials.password}`,
-      //  ...(todo._id ? { Id: `Basic ${todo._id}` } : {})
+        //  ...(todo._id ? { Id: `Basic ${todo._id}` } : {})
       },
       body: JSON.stringify(todo),
     })
   }
+  const Updater = (todo) => {
+    fetch("http://localhost:4000/todosupdate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${credentials.username}:${credentials.password}`,
+      },
+      body: JSON.stringify(todo),
+    })
+  }
+
   const handleToggle = (id) => {
-    const UpdatedTodo=[...Todos];
+    const UpdatedTodo = [...Todos];
     const todoItem = UpdatedTodo.find((todo) => todo._id === id);
     todoItem.Todos.checked = !todoItem.Todos.checked;
     setTodos(UpdatedTodo);
     LeChecker(todoItem);
   };
-  
-  
-  useEffect(()=>{Fetcher()},[]);
- 
+  const handleUpdate = (id, text, description, deadline) => {
+    const UpdatedTodo = [...Todos];
+    const todoItem = UpdatedTodo.find((todo) => todo._id === id);
+    todoItem.Todos.text = text;
+    todoItem.Todos.description = description;
+    todoItem.Todos.deadline = deadline;
+    setTodos(UpdatedTodo);
+    Updater(todoItem);
+  };
 
-    return (
-      
-        
-        <Container>
-            <DarkMode/>
-            <AddTodoForm onAddTodo={ handleAddTodo}
-            LePasseur={LePasseur}
-            />
-            <TodoList
-          todos={Todos}
-          onToggle={handleToggle}
-          onDelete={handleDelete}
-        />
+  useEffect(() => { Fetcher() }, []);
 
-        </Container>
-    
-    );
+
+  return (
+
+
+    <Container>
+      <DarkMode />
+      <AddTodoForm onAddTodo={handleAddTodo}
+        LePasseur={LePasseur}
+      />
+      <TodoList
+        todos={Todos}
+        onToggle={handleToggle}
+        onDelete={handleDelete}
+        onUpdate={handleUpdate}
+      />
+
+    </Container>
+
+  );
 }
 
 export default Todos;
